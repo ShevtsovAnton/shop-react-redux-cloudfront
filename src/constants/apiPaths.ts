b91c00e40156cc06cb0bc32ba505123c5f9485e0
ShevtsovAnton <a0660576041@gmail.com>
2024-11-17 19:10:59 +0200

    task-03

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,20 @@
-const API_PATHS = {
+const development = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
+
+const production = {
+  product: "https://50t8k9rb26.execute-api.us-east-1.amazonaws.com/prod",
+  order: "https://50t8k9rb26.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://g1tvclc2ml.execute-api.us-east-1.amazonaws.com/prod",
+  bff: "https://50t8k9rb26.execute-api.us-east-1.amazonaws.com/prod",
+  cart: "https://50t8k9rb26.execute-api.us-east-1.amazonaws.com/prod",
+}
+
+const API_PATHS = import.meta.env.MODE === 'production' ? production : development;
+
 
 export default API_PATHS;
